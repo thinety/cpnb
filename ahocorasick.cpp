1a54d5538882091fe9fc865732f2daa9c7ba6c30
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2024-09-15 20:21:27 -0300

    Update Aho-Corasick

--- ahocorasick.cpp
+++ ahocorasick.cpp
@@ -9,44 +9,44 @@ struct AhoCorasick {
         u32 output;
     };
 
-    vector<Node> nodes;
+    vector<Node> node;
 
     void build(vector<Word> const& words) {
         // root
-        nodes.emplace_back();
-        nodes.back().word = -1;
+        node.emplace_back();
+        node.back().word = -1;
 
         for (u32 w = 0; w < words.size(); ++w) {
             u32 u = 0;
             for (auto c : words[w]) {
-                if (nodes[u].next.count(c) == 0) {
-                    nodes[u].next.emplace(c, nodes.size());
-                    nodes.emplace_back();
-                    nodes.back().word = -1;
+                if (node[u].next.count(c) == 0) {
+                    node[u].next.emplace(c, node.size());
+                    node.emplace_back();
+                    node.back().word = -1;
                 }
-                u = nodes[u].next[c];
+                u = node[u].next[c];
             }
-            nodes[u].word = w;
+            node[u].word = w;
         }
 
         deque<u32> queue;
 
-        nodes[0].suffix = -1;
-        nodes[0].output = -1;
+        node[0].suffix = -1;
+        node[0].output = -1;
         queue.emplace_back(0);
 
         while (queue.size() > 0) {
             u32 u = queue.front(); queue.pop_front();
 
-            for (auto [c, v] : nodes[u].next) {
-                u32 suffix = nodes[u].suffix;
-                while (suffix != -1 && nodes[suffix].next.count(c) == 0) suffix = nodes[suffix].suffix;
-                if (suffix != -1) nodes[v].suffix = nodes[suffix].next[c];
-                else nodes[v].suffix = 0;
+            for (auto [c, v] : node[u].next) {
+                u32 suffix = node[u].suffix;
+                while (suffix != -1 && node[suffix].next.count(c) == 0) suffix = node[suffix].suffix;
+                if (suffix != -1) node[v].suffix = node[suffix].next[c];
+                else node[v].suffix = 0;
 
-                u32 output = nodes[v].suffix;
-                if (nodes[output].word != -1) nodes[v].output = output;
-                else nodes[v].output = nodes[output].output;
+                u32 output = node[v].suffix;
+                if (node[output].word != -1) node[v].output = output;
+                else node[v].output = node[output].output;
 
                 queue.emplace_back(v);
             }
@@ -54,19 +54,19 @@ struct AhoCorasick {
     }
 
     u32 go(u32 u, Char c) {
-        while (u != -1 && nodes[u].next.count(c) == 0) u = nodes[u].suffix;
-        if (u != -1) u = nodes[u].next[c];
+        while (u != -1 && node[u].next.count(c) == 0) u = node[u].suffix;
+        if (u != -1) u = node[u].next[c];
         else u = 0;
         return u;
     }
 
     template <typename F>
     void foreach_word(u32 u, F f) {
-        if (nodes[u].word != -1) f(nodes[u].word);
-        u = nodes[u].output;
+        if (node[u].word != -1) f(node[u].word);
+        u = node[u].output;
         while (u != -1) {
-            f(nodes[u].word);
-            u = nodes[u].output;
+            f(node[u].word);
+            u = node[u].output;
         }
     }
 };
